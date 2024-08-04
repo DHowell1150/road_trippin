83fa1f32e7533e1a450402aae0a43552f9241bab
Dana Howell <howelld115@gmail.com>
2024-08-03 19:21:18 -0600

    Completes WeatherService with tests

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -2,14 +2,13 @@ class WeatherService
   def self.call_api(endpoint, params = {})
     response = conn.get(endpoint, params) do |request|
       request.params = params
-      request.params[:key] = Rails.application.credentials[:weather][:key]
-      require 'pry' ; binding.pry
+      request.params[:key] = Rails.application.credentials.weather[:key]
     end
     parse_data(response)
   end
   
   def self.conn
-    Faraday.new(url: 'https://api.weatherapi.com/v1')
+    Faraday.new(url: 'https://api.weatherapi.com')
   end
 
   def self.parse_data(response)
--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -1,12 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe WeatherService do
+  it "can return current conditions for location" do
+    params = { q: "39.8705,-104.98552" }
+    query = WeatherService.call_api('/v1/forecast.json', params)
+
+    current = query[:current]
+    expect(current).to be_a Hash
+
+    expect(current).to have_key(:last_updated)
+    expect(current[:last_updated]).to be_a(String)
+
+    expect(current).to have_key(:temp_f)
+    expect(current[:temp_f]).to be_a(Float)
+
+    expect(current).to have_key(:condition)
+    expect(current[:condition]).to be_a(Hash)
+
+    expect(current[:condition]).to have_key(:icon)
+    expect(current[:condition][:icon]).to be_a(String)
+
+    expect(current).to have_key(:humidity)
+    expect(current[:humidity]).to be_a(Integer)
+
+    expect(current).to have_key(:uv)
+    expect(current[:uv]).to be_a(Float)
+  end
+
   it "can return forecast for location" do
     params = { q: "39.8705,-104.98552" }
-    query = WeatherService.call_api('/forecast.json', params)
-    # weather = query.first 
+    query = WeatherService.call_api('/v1/forecast.json', params)
+    
+    forecast = query.dig(:forecast, :forecastday, 0)
+    expect(forecast).to be_a Hash
 
-    # expect(query).to be_an Array
-    # expect(weather).to be_a Hash
+    expect(forecast).to have_key(:date)
+    expect(forecast[:date]).to be_a(String)
+
+    expect(forecast).to have_key(:astro)
+    expect(forecast[:astro]).to be_a(Hash)
+
+    expect(forecast[:astro]).to have_key(:sunrise)
+    expect(forecast[:astro][:sunrise]).to be_a(String)
+
+    expect(forecast[:astro]).to have_key(:sunset)
+    expect(forecast[:astro][:sunset]).to be_a(String)
+
+    expect(forecast).to have_key(:day)
+    expect(forecast[:day]).to be_a(Hash)
+
+    expect(forecast[:day]).to have_key(:maxtemp_f)
+    expect(forecast[:day][:maxtemp_f]).to be_a(Float)
+
+    expect(forecast[:day]).to have_key(:mintemp_f)
+    expect(forecast[:day][:mintemp_f]).to be_a(Float)
+
+    expect(forecast[:day][:condition]).to have_key(:text)
+    expect(forecast[:day][:condition][:text]).to be_a(String)
+
+    expect(forecast[:day][:condition]).to have_key(:icon)
+    expect(forecast[:day][:condition][:icon]).to be_a(String)
   end
 end
